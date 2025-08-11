5591b61ecef00e855bfc4c3d0701a1f84f81fcb4
German-source <german.pavlov.def@inbox.ru>
2025-08-11 13:24:34 +0400

    Update

---
+++ examples/TimerExample/TimerExample.h
@@ -0,0 +1,22 @@
+/* 
+ * Программа выводит время, прошедшее с момента запуска и
+ * моргает светодиодом на пине LED_BUILTIN.
+ */
+
+#include <TimerControl.h>                         // или #include <MicroOS.h>
+
+Timer tmr = 0;                                    // или timer32_t tmr = 0;
+
+void setup() {
+  Time.reset(tmr);                                // Необязательно
+  Time.mode(MILLIS);                              // Единицы измерения. MILLIS - по умолчанию
+  Serial.begin(9600);                             // Скорость передачи данных - 9600 бод
+  pinMode(LED_BUILTIN, OUTPUT);                   // Настройка пина
+}
+
+void loop() {
+  if (Time.control(tmr, 500)) {                   // Time.control(имя_таймера, период_установленных_единицах) - возвращает bool
+    digitalWrite(LED_BUILTIN, !digitalRead(13));  // Инвертируем пин
+    Serial.println(Time.read());                  // Выводим время                
+  }
+}
--- src/MicroOS.h
+++ src/MicroOS.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "TimerControl.h"
+#include 
